b83ca81c6243fa85774b66b6d627654523694624
Eric Pharr <ericjpharr@gmail.com>
2017-08-07 15:08:28 -0500

    Created albums table.

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+create table albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(128) NOT NULL,-- string for storing the recording artist name
+	name VARCHAR(255) NOT NULL,-- string for storing a record name
+	release_date YEAR,-- integer representing year record was released (Note: MySQL also supports a YEAR data type!)
+	sales FLOAT(8,6),-- floating point value for number of records sold (in millions)
+	genre TEXT,
+	PRIMARY KEY (id)-- string for storing the record's genre(s)
+);
