b7441c68088e216d786a3e76c16c7803a5de93e4
Eric Pharr <ericjpharr@gmail.com>
2017-08-08 15:39:36 -0500

    Ordered birthday/hire date query by the oldest employees / newest hire.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -21,7 +21,8 @@ order by emp_no DESC;
 select *
 from employees
 where birth_date like '%12-25'
-	and hire_date like '199%';
+	and hire_date like '199%'
+order by birth_date, hire_date DESC;
 
 select *
 from employees
