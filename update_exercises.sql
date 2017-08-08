ae1c9088cd60caf4cbb594c76d1574293061417b
Eric Pharr <ericjpharr@gmail.com>
2017-08-08 11:04:45 -0500

    Added / completed UPDATE exercise in MySQL.

---
+++ update_exercises.sql
@@ -0,0 +1,27 @@
+use codeup_test_db;
+
+pager less -~SFX;
+
+select * from albums;
+
+update albums
+set sales = sales * 10;
+
+select * from albums;
+
+select * from albums where release_date < 1980;
+
+update albums
+set release_date = release_date - 10
+where release_date < 1980;
+
+select * from albums where release_date < 1980;
+
+
+select * from albums where artist = "Michael Jackson";
+
+update albums
+set artist = "Peter Jackson"
+where artist = "Michael Jackson";
+
+select * from albums where artist = "Peter Jackson";
