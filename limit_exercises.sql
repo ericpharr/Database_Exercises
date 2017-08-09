a97063843eb4d39a436a7fa477e6ba99a08f3a54
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 10:38:24 -0500

    Corrected previous query and used offset to get 10th batch of 5 results.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -9,5 +9,5 @@ select first_name, last_name
 from employees
 where birth_date like '%12-25%'
   and hire_date like '199%'
-order by hire_date desc
-limit 5;
+order by birth_date, hire_date desc
+limit 5 offset 45;
