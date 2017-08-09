4fa73febff0a021734a9d4ceb0bb30915e64181c
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 10:31:17 -0500

    Added query for 12-25 birthdays by 5 most recent 90s hire date

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,10 @@ select distinct last_name
 from employees
 order by last_name desc
 limit 10;
+
+select first_name, last_name
+from employees
+where birth_date like '%12-25%'
+  and hire_date like '199%'
+order by hire_date desc
+limit 5;
