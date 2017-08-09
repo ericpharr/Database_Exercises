60c5be327a2a7afd3a5ee56eb51756bae53b3b96
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 09:39:13 -0500

    Added query to return unique last names containing 'q' but not 'qu'.

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,7 +12,13 @@ from employees
 where last_name like "e%e"
 group by last_name;
 
-select last_name
+select first_name, last_name
 from employees
 where last_name like "e%e"
 group by last_name, first_name;
+
+select last_name
+from employees
+where last_name like "%q%"
+  and last_name not like "%qu%"
+group by last_name;
