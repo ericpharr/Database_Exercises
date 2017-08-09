8432991064651b434f975f3ffa555341650e7596
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 17:02:10 -0500

    Bonus exercise for joins.

--- join_queries.sql
+++ join_queries.sql
@@ -35,16 +35,16 @@ join salaries as s
 	on s.emp_no = e.emp_no
 where dm.to_date > now() and s.to_date > now();
 
--- select concat(e.first_name, ' ', e.last_name) as `Employee Name`,
--- 	d.dept_name as `Department Name`,
--- 	concat(me.first_name, ' ', me.last_name) as `Manager Name`
--- from employees as e
--- join dept_emp as de
--- 	on de.emp_no = e.emp_no
--- join departments as d
--- 	on d.dept_no = de.dept_no
--- join dept_manager as dm
--- 	on dm.emp_no = de.emp_no
--- join employees as me
--- 	on me.emp_no as man = dm.emp_no
--- where dm.to_date > now()
+select concat(e.first_name, ' ', e.last_name) as `Employee Name`,
+	d.dept_name as `Department Name`,
+	concat(me.first_name, ' ', me.last_name) as `Manager Name`
+from employees as e
+join dept_emp as de
+	on de.emp_no = e.emp_no
+join departments as d
+	on d.dept_no = de.dept_no
+join dept_manager as dm
+	on dm.dept_no = de.dept_no
+join employees as me
+	on me.emp_no = dm.emp_no
+where dm.to_date > now() and de.to_date > now();
