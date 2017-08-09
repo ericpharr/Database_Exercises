e16598cfa51901989e67727ccedeaad04169f6c3
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 11:48:37 -0500

    Changed third query to calculated time worked at company.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -5,19 +5,12 @@ where (first_name = "Irena"
 	or first_name = 'Maya')
 group by gender;
 
-select *
-from employees
-where last_name like 'E%'
-	or last_name like '%e'
-order by emp_no DESC;
-
-select *
 select concat(first_name, ' ', last_name)
 from employees
 where last_name like 'E%e'
 order by emp_no DESC;
 
-select *
+select first_name, last_name, datediff(now(), hire_date)
 from employees
 where birth_date like '%12-25'
 	and hire_date like '199%'
