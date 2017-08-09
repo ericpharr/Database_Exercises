96fec4c878bc0bb7ce4d12e508ceb2773d2c1b9b
Eric Pharr <ericjpharr@gmail.com>
2017-08-09 11:38:40 -0500

    Refactored first query to find count of first names by gender.

---
+++ functions_exercises.sql
@@ -0,0 +1,29 @@
+select gender, count(*)
+from employees
+where (first_name = "Irena"
+	or first_name = 'Vidya'
+	or first_name = 'Maya')
+group by gender;
+
+select *
+from employees
+where last_name like 'E%'
+	or last_name like '%e'
+order by emp_no DESC;
+
+select *
+from employees
+where last_name like 'E%'
+	and last_name like '%e'
+order by emp_no DESC;
+
+select *
+from employees
+where birth_date like '%12-25'
+	and hire_date like '199%'
+order by birth_date, hire_date DESC;
+
+select *
+from employees
+where last_name like '%q%'
+	and last_name not like '%qu%';
